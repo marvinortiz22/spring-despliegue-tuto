7e3f7c1f7e9fc6cb0d5119261b867190a471eb94
marvinortiz22 <marvincubias22@gmail.com>
2024-03-19 23:08:45 -0600

    lectura de departamento

--- src/main/java/com/example/demo/endpoint.java
+++ src/main/java/com/example/demo/endpoint.java
@@ -1,16 +1,25 @@
 package com.example.demo;
 
+import com.example.demo.modelos.Departamento;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.example.demo.repositorios.departamentoRepositorio;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
 public class endpoint {
+
+    @Autowired
+    private departamentoRepositorio departamentoRepositorio;
     @GetMapping
     @ResponseBody
     public String endpoint(){
-        return "hola mundo";
+        Departamento departamento=departamentoRepositorio.findById(1).orElse(null);
+        return "el departamento con id 1 es: "+departamento.getNombreDepartamento();
     }
 }
